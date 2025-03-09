11ec95159dd5e8a1e5001fa7bffdd49ca3b0ec38
gangadhar2820 <gangadhar2820@gmail.com>
2025-03-09 10:46:42 +0530

    404 page updated

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Button } from "primereact/button";
+import { useNavigate } from "react-router-dom";
+import { transform } from "typescript";
 
 function PageNotFound() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <>
-    <div>PageNotFound</div>
-    <button onClick={()=>{navigate(-1)}}>Previous</button>
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "aliceblue",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,-50%)",
+          }}
+          className="flex flex-column align-items-center"
+        >
+          <img
+            src="/nechostelapp/images/aa.gif"
+            height={"300px"}
+            width={"300px"}
+            alt="404 logo"
+          ></img>
+          <h1 style={{fontFamily:"sans-serif"}}>OOPS! <i>Page not found</i></h1>
+
+          <Button text raised onClick={()=>{navigate(-1)}}><i className="pi pi-angle-double-left"></i> &nbsp;Go Back</Button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
